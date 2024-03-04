504405304c9c1d8c2e4831f251dd2e13fd5d3d36
AllySidd <161983200+AllySidd@users.noreply.github.com>
2024-03-05 04:45:19 +0500

    Add files via upload

---
+++ 15.Changing Guest List.js
@@ -0,0 +1,12 @@
+"use strict";
+let Guest_List = ['Asif ali zardari', 'nawaz sharif', 'Imran Khan'];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr.' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!\n\n');
+}
+let absent_Guest = 'maryam nawaz';
+let new_Guest = 'Kamran Tessori';
+Guest_List[0] = new_Guest;
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr.' + Guest_List[i] + ', \n\nIt is our pleasure to invite you in our party. \n\nThank You!\n\n');
+}
+console.log('Mr. ${absent_Guest} is not coming to the party.');
