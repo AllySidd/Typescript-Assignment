9785f80c6cd464dfad710630718d1ae4c91330bd
AllySidd <161983200+AllySidd@users.noreply.github.com>
2024-03-12 02:57:23 +0500

    Add files via upload

---
+++ 21.TypeScript_Objects.js
@@ -0,0 +1,19 @@
+"use strict";
+// Write a program that creates Objects containing these items.
+// person object.
+let person = {
+    age: 32,
+    name: 'Muhammad Ali Siddiqui',
+    nationaliy: 'Pakistan',
+    Student: true
+};
+// Print person.
+console.log(person);
+// Car Object.
+let Car = {
+    maker: 'Toyota',
+    color: 'White',
+    automatic: true
+};
+// Print person.
+console.log(Car);
