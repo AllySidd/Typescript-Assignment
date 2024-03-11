0754c39b7974548b26a60c780a736cf240555f42
AllySidd <161983200+AllySidd@users.noreply.github.com>
2024-03-12 02:58:49 +0500

    Add files via upload

---
+++ 23.Conditional_Test.js
@@ -0,0 +1,10 @@
+"use strict";
+// Write a series of conitional tests.
+// Creating car variable
+let car = 'subaru';
+// Test No 1
+console.log("Is car =='subaru'? I predict True.");
+console.log(car == 'subaru');
+// Test NO 2
+console.log("Is car == 'subaru'/ I predict True.");
+console.log(car == 'subaru');
