dae8738767c350b74094c251bf5a41d1301a3f22
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-20 05:08:38 +0500

    Delete 22.Intentional_Error.js

--- 22.Intentional_Error.js
+++
@@ -1,8 +0,0 @@
-// If you haven't received an array index error in one of your programs yet, try to make one happen.
-// Creating array.
-let newArray = ['Morgan Freeman', 'Arnold Schwarzenegger', 'Sylvester Stallone'];
-// Accesing array by index.
-console.log(newArray[5]);
-// Make Sure you correct the error before closing the program.
-// Correct the error
-console.log(newArray);
