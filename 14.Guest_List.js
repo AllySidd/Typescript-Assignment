c5222e8a0315f5d93068e333f5d88ae020ed4888
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-20 05:05:37 +0500

    Delete 14.Guest_List.js

--- 14.Guest_List.js
+++
@@ -1,5 +0,0 @@
-
-let Guest_List = ['Asif Ali Zardari', 'Nawaz Sharif', 'Imran Khan'];
-for (let i = 0; i < Guest_List.length; i++) {
-    console.log('Dear Mr.' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!');
-}
