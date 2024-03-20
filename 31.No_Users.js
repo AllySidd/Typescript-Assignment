f1e6ac12be4f3e586ddb7c9c3103d165371ae42e
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-20 05:12:13 +0500

    Delete 31.No_Users.js

--- 31.No_Users.js
+++
@@ -1,8 +0,0 @@
-let usernames = [];
-if (usernames.length === 0) {
-    console.log("We need to find some users!");
-}
-else {
-    //Greet users
-}
-//Removing all usernames ensures the message "We need to find some users!" is printed.
