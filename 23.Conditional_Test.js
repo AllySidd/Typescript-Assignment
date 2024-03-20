827afae6799cf3711cf8d651124ee10a9c864d21
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-20 05:09:01 +0500

    Delete 23.Conditional_Test.js

--- 23.Conditional_Test.js
+++
@@ -1,9 +0,0 @@
-// Write a series of conitional tests.
-// Creating car variable
-let car = 'subaru';
-// Test No 1
-console.log("Is car =='subaru'? I predict True.");
-console.log(car == 'subaru');
-// Test NO 2
-console.log("Is car == 'subaru'/ I predict True.");
-console.log(car == 'subaru');
