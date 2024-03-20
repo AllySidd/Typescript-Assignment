1e0a514199a6fa6db56af3d6e27922718003ed95
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-20 05:17:24 +0500

    Add files via upload

---
+++ 12.Greeting/Name.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Muhammad Ali Siddiqui', 'Anum', 'Talha', 'Hussain'];
+const message = 'Welcome to Typescript class';
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 12.Greeting/Name.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] = ['Muhammad Ali Siddiqui','Anum','Talha','Hussain'];
+
+const message :string ='Welcome to Typescript class:'
+
+for(let i=0; i<names.length; i++){
+
+    console.log(names[i]) ;
+
+}
