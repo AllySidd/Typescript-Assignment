aa0e25a99c9ba5fafe52a6366a872227857ea5a0
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-21 22:28:06 +0500

    Update Your_Own_Array.js

--- 13.Your Own Array/Your_Own_Array.js
+++ 13.Your Own Array/Your_Own_Array.js
@@ -1,4 +1,3 @@
-"use strict";
 let transportation = ['motorcycle', 'car', 'Train', 'Cycle'];
 for (let i = 0; i < transportation.length; i++) {
     console.log('I would like to own a ' + transportation[i]);
