b249457580be460000347365447d9f2cb1af90f7
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-21 22:30:38 +0500

    Update Famous_Quote_2.js

--- 5.Famous Quote2/Famous_Quote_2.js
+++ 5.Famous Quote2/Famous_Quote_2.js
@@ -1,4 +1,3 @@
-"use strict";
 let famous_person = 'Albert Einstein';
 let message = '"A person who never made a mistake never tried anything new."';
 console.log(`${famous_person} once said, ${message}`);
