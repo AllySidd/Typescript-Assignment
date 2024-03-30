45778b4894f1429f8c7c18356b25d67677baeda5
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-30 05:24:24 +0500

    Delete 6.Stripping Names directory

--- 6.Stripping Names/Stripping_Name.js
+++
@@ -1,4 +0,0 @@
-const personName = '   \n\tshoaib akhtar\t\n    ';
-console.log(personName);
-const without_whitespace = personName.trim();
-console.log(without_whitespace);
--- 6.Stripping Names/Stripping_Name.ts
+++
@@ -1,9 +0,0 @@
-
-
-const personName :string ='   \n\tshoaib akhtar\t\n    ';
-
-console.log(personName);
-
-const without_whitespace :string = personName.trim();
-
-console.log(without_whitespace);
