45c6a4924574d51275d6ee6939cac25be36ae222
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-31 03:38:53 +0500

    Delete 13.Your Own Array directory

--- 13.Your Own Array/Your_Own_Array.ts
+++
@@ -1,9 +0,0 @@
-
-
-let transportation :string[] = ['motorcycle','car','Train','Cycle'] ;
-
-for(let i=0; i<transportation.length; i++){
-
-    console.log('I would like to own a ' + transportation[i])
-    
-}
