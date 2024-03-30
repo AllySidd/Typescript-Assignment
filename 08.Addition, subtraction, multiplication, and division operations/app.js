d408cbdb518b4b2322c8acb051e2e796f6dbd99a
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-31 04:56:04 +0500

    Add files via upload

---
+++ 08.Addition, subtraction, multiplication, and division operations/app.js
@@ -0,0 +1,22 @@
+/*Question # 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result
+in the number 8. Be sure to enclose your operations in print statements to see the results.*/
+//addition operation
+let a = 6;
+let b = 2;
+let sum = a + b;
+console.log(`Addition operation: ${a}+${b}=${sum}`);
+//subtraction operation
+let c = 12;
+let d = 4;
+let sub = c - d;
+console.log(`Subtraction operation:${c}-${d}=${sub}`);
+//multiplication operation
+let e = 2;
+let f = 4;
+let mul = e * f;
+console.log(`Multiplication operation: ${e}*${f}=${mul}`);
+//division operation
+let g = 32;
+let h = 4;
+let div = g / h;
+console.log(`Division operation: ${g}/${h}=${div}`);
