1c6adc7b9a545ab3b3fa5da2fef6efca7d550599
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-30 05:30:26 +0500

    Add files via upload

---
+++ 02.Personal Message/2.PersonalMessage.js
@@ -0,0 +1,8 @@
+let personName = '';
+personName = prompt('What is your Name?') || '';
+if (personName !== null && personName !== '') {
+    alert('Hello $(personName), would you like to learn some Python today?');
+}
+else {
+    alert('You have to Fill your name !');
+}
