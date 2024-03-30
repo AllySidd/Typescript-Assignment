3dd2c63a761f381dafc7ca65b5a413cf720fdf15
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-30 05:25:41 +0500

    Delete 10.Adding Comments directory

--- 10.Adding Comments/Program_1.js
+++
@@ -1,5 +0,0 @@
-// Program Written by: Muhammad Ali Siddiqui
-// Current Date: 19-2-2024
-// This program will write Hello World
-console.log('Hello World');
-console.log('Muhammad Ali Siddiqui');
--- 10.Adding Comments/Program_2.ts
+++
@@ -1,8 +0,0 @@
-
-
-// Program Written by: Muhammad Ali Siddiqui
-// Current Date: 19-2-2004
-
-//This program will two digits.
-
-console.log(2+2);
