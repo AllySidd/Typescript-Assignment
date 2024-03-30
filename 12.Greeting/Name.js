d37007af2b1d320ba988f24286ee0e824b58728f
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-31 03:38:10 +0500

    Delete 12.Greeting directory

--- 12.Greeting/Name.js
+++
@@ -1,5 +0,0 @@
-let names = ['Muhammad Ali Siddiqui', 'Anum', 'Talha', 'Hussain'];
-const message = 'Welcome to Typescript class';
-for (let i = 0; i < names.length; i++) {
-    console.log(names[i]);
-}
