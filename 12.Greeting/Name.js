2d071e7191294615ffc34fb682dbb9f58702c59d
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-31 05:12:06 +0500

    Add files via upload

---
+++ 12.Greeting/Name.js
@@ -0,0 +1,5 @@
+let names = ['Muhammad Ali Siddiqui', 'Anum', 'Talha', 'Hussain'];
+const message = 'Welcome to Typescript class';
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
