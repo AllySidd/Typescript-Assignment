8957c8de485239ec4e070b292540ed706300911b
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-31 05:14:27 +0500

    Add files via upload

---
+++ 14.Guest List/Guest_List.js
@@ -0,0 +1,4 @@
+let Guest_List = ['Asif Ali Zardari', 'Nawaz Sharif', 'Imran Khan'];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr.' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!');
+}
