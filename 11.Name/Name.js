898b9cdad836dffe0871c02486adf87c05d03bfe
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-03-31 05:10:58 +0500

    Add files via upload

---
+++ 11.Name/Name.js
@@ -0,0 +1,4 @@
+let names = ['Muhammad Ali Siddiqui', 'Anum', 'Talha', 'Hussain'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11.Name/Name.ts
@@ -0,0 +1,8 @@
+
+
+let names :string[] = ['Muhammad Ali Siddiqui','Anum','Talha','Hussain'];
+
+for(let i=0; i<names.length; i++){
+
+    console.log(names[i]) ;
+}
