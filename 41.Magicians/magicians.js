a406a8404b7d0276eab97f844f7c9717a9b6648b
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-04-05 04:17:21 +0500

    Add files via upload

---
+++ 41.Magicians/magicians.js
@@ -0,0 +1,7 @@
+let magicians = ["David Blaine", "Darren Brown", "Criss Angel", "David Copperfield"];
+function show_magicians(magicians) {
+    magicians.forEach(magicians => {
+        console.log(magicians);
+    });
+}
+show_magicians(magicians);
