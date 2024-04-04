a1ea71652a1f46ec7fd2a2f22bfe9ace7ee5142d
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-04-05 04:14:46 +0500

    Add files via upload

---
+++ 38.Cities/cities.js
@@ -0,0 +1,7 @@
+// Question # 38: Cities
+function describe_city(city, country = "Pakistan") {
+    console.log(`${city} is in ${country}`);
+}
+describe_city("Karachi");
+describe_city("Lahore");
+describe_city("Tokyo", "Japan");
