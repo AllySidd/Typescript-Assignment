2274aaee420f637e393fae18df28c79fad6dd959
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-04-05 03:43:45 +0500

    Add files via upload

---
+++ 24.More Conditional Tests/More_Conditional_Tests.js
@@ -0,0 +1,9 @@
+// Equality and Inequality Test 1
+console.log("Equality test with strings: ", "Apple" === "Apple");
+// Equality and Inequality Test 2
+console.log("Inequality test with strigs: ", "Apple" != "orange");
+// Test using the lower case function
+console.log("Lower Case function test: ", "HELLO".toLowerCase() === "hello");
+// Numerical tests involving equality and inequality
+console.log("Equality test with numbers: ", 26 === 26);
+//, grater than and less than, greater than or equal to, and less than or equal to
