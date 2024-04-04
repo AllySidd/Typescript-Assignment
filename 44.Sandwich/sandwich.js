e76c764c96e43427c5687447087d8ba713af5615
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-04-05 04:48:48 +0500

    Add files via upload

---
+++ 44.Sandwich/sandwich.js
@@ -0,0 +1,6 @@
+function make_sandwich(...items) {
+    console.log(`Making a sandwich with: ${items.join(',')}.`);
+}
+make_sandwich("chicken", "cheese");
+make_sandwich("egg", "mayo", "special Spicy Sauce");
+make_sandwich("green Sauce", "cheesy chicken Spread", "lettuce");
