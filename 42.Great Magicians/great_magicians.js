66a9232e50db286798c29d168314153a6b868ede
MAS™️ <161983200+AllySidd@users.noreply.github.com>
2024-04-05 04:47:13 +0500

    Add files via upload

---
+++ 42.Great Magicians/great_magicians.js
@@ -0,0 +1,14 @@
+//Question # 42. Great Magicians
+let magicians = ["Alice", "David", "Chris"];
+function show_magicians(magicians) {
+    magicians.forEach((magician) => {
+        console.log(magician);
+    });
+}
+function make_great(magicians) {
+    for (let i = 0; i < magicians.length; i++) {
+        magicians[i] = magicians[i] + " the Great";
+    }
+}
+make_great(magicians);
+show_magicians(magicians);
